761a092f58a50875195bec3a45149c7c97fb07fa
jarji <100996174+jarjia@users.noreply.github.com>
2023-03-22 20:57:50 +0400

    feat: change posts file

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,15 +5,15 @@
 
     <body>
         <article>
-            <h1><a href='/post'>my first blog</a></h1>
+            <h1><a href='/posts/my-first-post'>my first blog</a></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lorem sapien, pellentesque non posuere eu, elementum consectetur augue. Fusce dignissim purus lorem, non congue nunc pulvinar quis. Suspendisse scelerisque commodo metus vel iaculis. Nullam imperdiet tincidunt neque non pharetra. Fusce congue augue condimentum quam malesuada, non elementum purus laoreet. Curabitur a varius ligula, vulputate pretium libero. Pellentesque convallis ultrices quam ut facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
         </article>
         <article>
-            <h1><a href='/post'>my second blog</a></h1>
+            <h1><a href='/posts/my-second-post'>my second blog</a></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lorem sapien, pellentesque non posuere eu, elementum consectetur augue. Fusce dignissim purus lorem, non congue nunc pulvinar quis. Suspendisse scelerisque commodo metus vel iaculis. Nullam imperdiet tincidunt neque non pharetra. Fusce congue augue condimentum quam malesuada, non elementum purus laoreet. Curabitur a varius ligula, vulputate pretium libero. Pellentesque convallis ultrices quam ut facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
         </article>
         <article>
-            <h1><a href='/post'>my third blog</a></h1>
+            <h1><a href='/posts/my-third-post'>my third blog</a></h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lorem sapien, pellentesque non posuere eu, elementum consectetur augue. Fusce dignissim purus lorem, non congue nunc pulvinar quis. Suspendisse scelerisque commodo metus vel iaculis. Nullam imperdiet tincidunt neque non pharetra. Fusce congue augue condimentum quam malesuada, non elementum purus laoreet. Curabitur a varius ligula, vulputate pretium libero. Pellentesque convallis ultrices quam ut facilisis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
         </article>
     </body>
