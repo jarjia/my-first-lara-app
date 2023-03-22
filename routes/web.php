788b222e5be70c0ea28435a1652491de03633613
jarji <100996174+jarjia@users.noreply.github.com>
2023-03-22 20:59:50 +0400

    feat: add new route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('/post', function () {
-    return view('post');
+
+Route::get('/posts/{post}', function ($slug) {
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+
+    if(! file_exists($path)) {
+        // dd('file does not exist');
+        // ddd('file does not exist');
+        // abort(404);
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
 });
